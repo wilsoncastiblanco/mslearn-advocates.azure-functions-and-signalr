b37d2e7e2e1fa13501c685a3bab2d5fa7c5824c1
Wilson Castiblanco Quintero <wcastiblancoq@gmail.com>
2025-02-22 09:14:45 -0600

    Update deployment workflow to use package path

--- start/server/src/functions/signalr-open-connection.ts
+++ start/server/src/functions/signalr-open-connection.ts
@@ -1,15 +1,17 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { app, input } from '@azure/functions';
 
-export async function signalr-open-connection(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+const inputSignalR = input.generic({
+    type: 'signalRConnectionInfo',
+    name: 'connectionInfo',
+    hubName: 'default',
+    connectionStringSetting: 'SIGNALR_CONNECTION_STRING',
+});
 
-    const name = request.query.get('name') || await request.text() || 'world';
-
-    return { body: `Hello, ${name}!` };
-};
-
-app.http('signalr-open-connection', {
-    methods: ['GET', 'POST'],
+app.http('open-signalr-connection', {
     authLevel: 'anonymous',
-    handler: signalr-open-connection
+    handler: (request, context) => {
+        return { body: JSON.stringify(context.extraInputs.get(inputSignalR)) }
+    },
+    route: 'negotiate',
+    extraInputs: [inputSignalR]
 });
